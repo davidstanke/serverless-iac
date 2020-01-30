23ef8631cfbb8ffe45ac3eeae56af68195ef3dd8
Dave Stanke <davidstanke@google.com>
2020-01-30 13:49:30 -0500

    make url output a list

--- variables.tf
+++ variables.tf
@@ -1,11 +1,14 @@
 variable "project_id" {
   type = string
+  default="iac-for-serverless-staging"
 }
 
 variable "service_names" {
   type = list(string)
+  default=["onfirst","onsecond","onthird","roster"]
 }
 
 variable "image_tag" {
   type = string
+  default = "test"
 }
