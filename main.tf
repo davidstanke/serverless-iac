d641a94d60a99541f9886bae6be0a7ab7fb7fa90
Dave Stanke <davidstanke@gmail.com>
2020-02-11 12:25:46 -0500

    mild refactor

--- main.tf
+++ main.tf
@@ -2,8 +2,8 @@ provider "google" {
   project = var.project-id
 }
 
-module "deploy-service" {
-  source = "./modules/deploy-service"
+module "deploy-cloudrun" {
+  source = "./modules/deploy-cloudrun"
   image-tag = var.image-tag
   service-names = var.service-names
   project-id = var.project-id
