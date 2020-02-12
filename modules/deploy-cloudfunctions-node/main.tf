1d2356e47798413e160f2c87285ff2db829d1a75
Dave Stanke <davidstanke@google.com>
2020-02-12 01:49:50 -0500

    disable GCF auth resource again

--- modules/deploy-cloudfunctions-node/main.tf
+++ modules/deploy-cloudfunctions-node/main.tf
@@ -30,12 +30,12 @@ resource "google_cloudfunctions_function" "myfunction" {
   trigger_http          = true
 }
 
-# IAM entry for all users to invoke the function
-resource "google_cloudfunctions_function_iam_member" "invoker" {
-  project        = google_cloudfunctions_function.myfunction.project
-  region         = google_cloudfunctions_function.myfunction.region
-  cloud_function = google_cloudfunctions_function.myfunction.name
+# # IAM entry for all users to invoke the function
+# resource "google_cloudfunctions_function_iam_member" "invoker" {
+#   project        = google_cloudfunctions_function.myfunction.project
+#   region         = google_cloudfunctions_function.myfunction.region
+#   cloud_function = google_cloudfunctions_function.myfunction.name
 
-  role   = "roles/cloudfunctions.invoker"
-  member = "allUsers"
-}
+#   role   = "roles/cloudfunctions.invoker"
+#   member = "allUsers"
+# }
